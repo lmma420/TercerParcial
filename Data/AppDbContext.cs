2223e3306ea118f6e55abc435dc9630cc06a783f
Luis Mario Mercado Antelo <a2019116634@estudiantes.upsa.edu.bo>
2021-11-14 22:23:09 -0400

    pollo maldito pollo infernal

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TercerParcial.Models;
+
+namespace TercerParcial.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Puntos> Puntos { get; set; }
+    }
+}
