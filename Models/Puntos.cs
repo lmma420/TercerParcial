b8d73e03c732b014f9c0a183e59e43e979ba14ba
Luis Mario Mercado Antelo <a2019116634@estudiantes.upsa.edu.bo>
2021-11-14 22:06:12 -0400

    dios mio

---
+++ Models/Puntos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TercerParcial.Models
+{
+    public class Puntos
+    {
+        [Key]
+        public string nombre { get; set; }
+        public string ubi { get; set; }
+        public string desc { get; set; }
+        public int num { get; set; }
+        public string fotos { get; set; }
+
+
+    }
+}
