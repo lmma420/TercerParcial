67f2f5d2c18ff3674cbe1e9a531de47a8b84b352
Luis Mario Mercado Antelo <a2019116634@estudiantes.upsa.edu.bo>
2021-11-14 22:41:34 -0400

    BASE DE DATO CREADA MI REY

---
+++ Migrations/20211115023453_AddModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TercerParcial.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Puntos",
+                columns: table => new
+                {
+                    nombre = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ubi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    num = table.Column<int>(type: "int", nullable: false),
+                    fotos = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Puntos", x => x.nombre);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Puntos");
+        }
+    }
+}
